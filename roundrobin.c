45a20f5460f760e704f82d24de368199eaf1afd8
Sajal Sharma <124279092+sajal703@users.noreply.github.com>
2023-09-16 12:21:57 +0530

    Update roundrobin.c

--- roundrobin.c
+++ roundrobin.c
@@ -1,61 +1,69 @@
-#include <stdio.h>
-
-int main() {
-  int n, burst_time[100], arrival_time[100], quantum;
-  float turnaround_time[100], waiting_time[100];
-
-  printf("Enter the number of processes: ");
-  scanf("%d", &n);
-
-  for (int i = 0; i < n; i++) {
-    printf("Enter the arrival time and burst time for process %d: ", i + 1);
-    scanf("%d %d", &arrival_time[i], &burst_time[i]);
-  }
-
-  printf("Enter the time quantum: ");
-  scanf("%d", &quantum);
-
-  for (int i = 0; i < n; i++) {
-    turnaround_time[i] = burst_time[i];
-    waiting_time[i] = 0;
-  }
-
-  int current_time = 0;
-  while (1) {
-    int process_to_execute = -1;
-    for (int i = 0; i < n; i++) {
-      if (arrival_time[i] <= current_time && burst_time[i] > 0) {
-        process_to_execute = i;
-        break;
+#include<stdio.h>
+ 
+int main()
+{
+      int i, limit, total = 0, x, counter = 0, time_quantum;
+      int wait_time = 0, turnaround_time = 0, arrival_time[10], burst_time[10], temp[10];
+      float average_wait_time, average_turnaround_time;
+      printf("nEnter Total Number of Processes:t");
+      scanf("%d", &limit);
+      x = limit;
+      for(i = 0; i < limit; i++)
+      {
+            printf("nEnter Details of Process[%d]n", i + 1);
+ 
+            printf("Arrival Time:t");
+ 
+            scanf("%d", &arrival_time[i]);
+ 
+            printf("Burst Time:t");
+ 
+            scanf("%d", &burst_time[i]);
+ 
+            temp[i] = burst_time[i];
       }
-    }
-
-    if (process_to_execute == -1) {
-      break;
-    }
-
-    int time_left = (burst_time[process_to_execute] > quantum) ? quantum : burst_time[process_to_execute];
-    burst_time[process_to_execute] -= time_left;
-    waiting_time[process_to_execute] += (current_time - arrival_time[process_to_execute]);
-    current_time += time_left;
-
-    if (burst_time[process_to_execute] == 0) {
-      turnaround_time[process_to_execute] = current_time;
-    }
-  }
-
-  float average_turnaround_time = 0.0;
-  float average_waiting_time = 0.0;
-  for (int i = 0; i < n; i++) {
-    average_turnaround_time += turnaround_time[i];
-    average_waiting_time += waiting_time[i];
-  }
-
-  average_turnaround_time /= n;
-  average_waiting_time /= n;
-
-  printf("Average turnaround time: %f\n", average_turnaround_time);
-  printf("Average waiting time: %f\n", average_waiting_time);
-
-  return 0;
+ 
+      printf("nEnter Time Quantum:t");
+      scanf("%d", &time_quantum);
+      printf("nProcess IDttBurst Timet Turnaround Timet Waiting Timen");
+      for(total = 0, i = 0; x != 0;)
+      {
+            if(temp[i] <= time_quantum && temp[i] > 0)
+            {
+                  total = total + temp[i];
+                  temp[i] = 0;
+                  counter = 1;
+            }
+            else if(temp[i] > 0)
+            {
+                  temp[i] = temp[i] - time_quantum;
+                  total = total + time_quantum;
+            }
+            if(temp[i] == 0 && counter == 1)
+            {
+                  x--;
+                  printf("nProcess[%d]tt%dtt %dttt %d", i + 1, burst_time[i], total - arrival_time[i], total - arrival_time[i] - burst_time[i]);
+                  wait_time = wait_time + total - arrival_time[i] - burst_time[i];
+                  turnaround_time = turnaround_time + total - arrival_time[i];
+                  counter = 0;
+            }
+            if(i == limit - 1)
+            {
+                  i = 0;
+            }
+            else if(arrival_time[i + 1] <= total)
+            {
+                  i++;
+            }
+            else
+            {
+                  i = 0;
+            }
+      }
+ 
+      average_wait_time = wait_time * 1.0 / limit;
+      average_turnaround_time = turnaround_time * 1.0 / limit;
+      printf("nnAverage Waiting Time:t%f", average_wait_time);
+      printf("nAvg Turnaround Time:t%fn", average_turnaround_time);
+      return 0;
 }
